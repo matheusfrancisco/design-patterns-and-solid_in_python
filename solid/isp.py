36edb7ec90cc15f7fe3801f36bd1b0fda0b09613
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-09-12 21:55:39 -0300

    interface segregation exemple

---
+++ solid/isp.py
@@ -0,0 +1,82 @@
+# Interface Segregation Principle
+import abc
+
+
+class Machine:
+    def print(self, document):
+        raise NotImplementedError
+
+    def fax(self, document):
+        raise NotImplementedError
+
+    def scan(self, document):
+        raise NotImplementedError
+
+
+class MultiFunctionPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass # noop
+
+    def scan(self, document):
+        raise NotImplementedError('Printer cannot scan!')
+
+
+class Printer:
+    @abc.abstractmethod
+    def print(self, document):
+        pass
+
+
+class Scanner:
+    @abc.abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MyPrinter(Printer):
+    def print(self, document):
+        print(document)
+
+
+class Photocopier(Printer, Scanner):
+    def print(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionDevice(Printer, Scanner):
+    @abc.abstractmethod
+    def print(self, document):
+        pass
+
+    @abc.abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionMachine(MultiFunctionDevice):
+    def __init__(self, printer, scanner):
+        self.printer = printer
+        self.scanner = scanner
+
+    def print(self, document):
+        self.printer.print(document)
+
+    def scan(self, document):
+        self.scanner.scan(document)
