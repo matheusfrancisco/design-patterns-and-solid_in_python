59562c109a2f9f3e540e3ecad0b7f7db7fadf202
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-10-19 16:27:45 -0300

    Add chain ds patterns

---
+++ design-patterns-course-exercises/chain/README.md
@@ -0,0 +1,12 @@
+## Chain of responsibility pattern
+
+As the name suggests, the chain of responsibility pattern creates a chain of receiver objects for a request.
+This pattern decouples sender and receiver of a request based on type of request.
+This pattern comes under behavioral patterns.
+
+In this pattern, normally each receiver contains reference to another receiver.
+If one object cannot handle the request then it passes the same to the next receiver and so on.
+
+# Reference
+
+https://refactoring.guru/design-patterns/chain-of-responsibility/python/example
---
+++ design-patterns-course-exercises/chain/calculador_de_descontos.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+from descontos import Desconto_por_cinco_itens, Desconto_por_mais_de_quinhentos_reais, Sem_desconto
+
+class Calculador_de_descontos:
+
+	def calcula(self, orcamento):
+		desconto = Desconto_por_cinco_itens(
+			Desconto_por_mais_de_quinhentos_reais(Sem_desconto())
+		).calcula(orcamento)
+
+		return desconto
+
+if __name__ == '__main__':
+
+	from orcamento import Orcamento, Item
+
+	orcamento = Orcamento()
+	orcamento.adiciona_item(Item('ITEM - 1', 100))
+	orcamento.adiciona_item(Item('ITEM - 2', 50))
+	orcamento.adiciona_item(Item('ITEM - 3', 400))
+	calculador = Calculador_de_descontos()
+	desconto = calculador.calcula(orcamento)
